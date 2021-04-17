267acf6066a581c687946f232b9a829dbb63f7fb
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-04-17 15:15:34 +0300

    Wrote small program that checks user input type.

---
+++ HelloWorld2/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    class Box
+    {
+        public int Heigth { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public int Surface 
+        {
+            get
+            {
+                return this.Heigth * this.Length;
+            }
+        }
+
+        public void Volume()
+        {
+            Console.WriteLine($"Height {Heigth}, length {Length}, width {Width}, volume {Heigth * Length * Width}.");
+        }
+    }
+}
--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -10,20 +10,56 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
-            Human basicBitch = new Human();
-            basicBitch.SayMyName();
+            // Small program that checks user input type.
 
-            Human level1Bitch = new Human("Bitchadra");
-            level1Bitch.SayMyName();
+            Console.Write("Enter any value, quickly: ");
 
-            Human level2Bitch = new Human("Bitchadra", "Viagrosa");
-            level2Bitch.SayMyName();
+            string userInput = Console.ReadLine();
 
-            Human level3Bitch = new Human("Bitchadra", "Viagrosa", 69);
-            level3Bitch.SayMyName();
+            Console.WriteLine("Now select the correct data type of your input:");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
 
-            Human bitchSupreme = new Human("Bitchadra", "Viagrosa", 69, true);
-            bitchSupreme.SayMyName();
+            bool selectionCorrect = false;
+            string dataType = string.Empty;
+
+            while (!selectionCorrect)
+            {
+                dataType = Console.ReadLine();
+
+                if (dataType == "1" || dataType == "2" || dataType == "3")
+                {
+                    selectionCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Are you some kind of re-tard?");
+                    Console.WriteLine("Select the correct data type of your input:");
+                    Console.WriteLine("Press 1 for String");
+                    Console.WriteLine("Press 2 for Integer");
+                    Console.WriteLine("Press 3 for Boolean");
+                }
+            }
+
+            string message;
+
+            switch (dataType)
+            {
+                case "1":
+                    message = !userInput.Any(char.IsDigit)
+                        ? "Correct. Your input is indeed a string."
+                        : "Technically a string, but your input has at least one digit, so no.";
+                    break;
+                case "2":
+                    message = int.TryParse(userInput, out int intValue) ? "Correct. Your input is indeed an integer." : "Nope.";
+                    break;
+                default:
+                    message = bool.TryParse(userInput, out bool boolValue) ? "Correct. Your input is indeed a boolean." : "Nope.";
+                    break;
+            }
+
+            Console.WriteLine(message);
 
             Console.Read();
         }
