78aa547d3b4ecbafdbe25e8016ab47c7d9075b0a
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-04-20 16:06:41 +0300

    Made a simple 2 player tic tac toe game, no AI.

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -10,58 +10,136 @@ namespace HelloWorld2
     {
         static void Main(string[] args)
         {
-            // Small program that checks user input type.
+            // 2 player tic tac toe game, no AI.
 
-            Console.Write("Enter any value, quickly: ");
+            int move;
+            bool player1Move = true;
+            bool gameIsOn = true;
+            string[] ticTacToeGrid = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-            string userInput = Console.ReadLine();
+            DrawGrid(ticTacToeGrid);
 
-            Console.WriteLine("Now select the correct data type of your input:");
-            Console.WriteLine("Press 1 for String");
-            Console.WriteLine("Press 2 for Integer");
-            Console.WriteLine("Press 3 for Boolean");
-
-            bool selectionCorrect = false;
-            string dataType = string.Empty;
-
-            while (!selectionCorrect)
+            while (gameIsOn)
             {
-                dataType = Console.ReadLine();
+                Console.WriteLine("To make a move type an empty square number from 1 to 9.");
 
-                if (dataType == "1" || dataType == "2" || dataType == "3")
-                {
-                    selectionCorrect = true;
-                }
-                else
-                {
-                    Console.WriteLine("Are you some kind of re-tard?");
-                    Console.WriteLine("Select the correct data type of your input:");
-                    Console.WriteLine("Press 1 for String");
-                    Console.WriteLine("Press 2 for Integer");
-                    Console.WriteLine("Press 3 for Boolean");
-                }
+                string whoseTurn = player1Move ? "Player 1 turn: " : "Player 2 turn: ";
+
+                Console.Write(whoseTurn);
+
+                move = CheckMove(ticTacToeGrid);
+
+                MakeMove(ticTacToeGrid, move, ref player1Move);
+                DrawGrid(ticTacToeGrid);
+                CheckWinner(ref ticTacToeGrid, ref player1Move, ref gameIsOn);
+                CheckDraw(ref ticTacToeGrid, ref player1Move, ref gameIsOn);
             }
-
-            string message;
-
-            switch (dataType)
-            {
-                case "1":
-                    message = !userInput.Any(char.IsDigit)
-                        ? "Correct. Your input is indeed a string."
-                        : "Technically a string, but your input has at least one digit, so no.";
-                    break;
-                case "2":
-                    message = int.TryParse(userInput, out int intValue) ? "Correct. Your input is indeed an integer." : "Nope.";
-                    break;
-                default:
-                    message = bool.TryParse(userInput, out bool boolValue) ? "Correct. Your input is indeed a boolean." : "Nope.";
-                    break;
-            }
-
-            Console.WriteLine(message);
 
             Console.Read();
+        }
+
+        static void DrawGrid(string[] grid)
+        {
+            Console.Clear();
+            Console.WriteLine("-------------");
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                Console.Write($"| {grid[i]} ");
+
+                if ((i + 1) % 3 == 0)
+                {
+                    Console.Write("|");
+                    Console.WriteLine();
+                    Console.WriteLine("-------------");
+                }
+            }
+        }
+
+        static int CheckMove(string[] grid)
+        {
+            string playerMove = Console.ReadLine();
+            bool validMove = int.TryParse(playerMove, out int move) && grid.Contains(playerMove);
+
+            while (!validMove)
+            {
+                Console.WriteLine("Invalid move, try again.");
+                playerMove = Console.ReadLine();
+                validMove = int.TryParse(playerMove, out move) && grid.Contains(playerMove);
+            }
+
+            return move;
+        }
+
+        static void MakeMove(string[] grid, int move, ref bool player1Move)
+        {
+            if (player1Move)
+            {
+                grid[move - 1] = "X";
+                player1Move = false;
+            }
+            else
+            {
+                grid[move - 1] = "O";
+                player1Move = true;
+            }
+        }
+
+        static void CheckWinner(ref string[] grid, ref bool player1Move, ref bool gameIsOn)
+        {
+            bool row1 = grid[0] == "X" && grid[1] == "X" && grid[2] == "X" || grid[0] == "O" && grid[1] == "O" && grid[2] == "O";
+            bool row2 = grid[3] == "X" && grid[4] == "X" && grid[5] == "X" || grid[3] == "O" && grid[4] == "O" && grid[5] == "O";
+            bool row3 = grid[6] == "X" && grid[7] == "X" && grid[8] == "X" || grid[6] == "O" && grid[7] == "O" && grid[8] == "O";
+
+            bool col1 = grid[0] == "X" && grid[3] == "X" && grid[6] == "X" || grid[0] == "O" && grid[3] == "O" && grid[6] == "O";
+            bool col2 = grid[1] == "X" && grid[4] == "X" && grid[7] == "X" || grid[1] == "O" && grid[4] == "O" && grid[7] == "O";
+            bool col3 = grid[2] == "X" && grid[5] == "X" && grid[8] == "X" || grid[2] == "O" && grid[5] == "O" && grid[8] == "O";
+
+            bool diagonal1 = grid[0] == "X" && grid[4] == "X" && grid[8] == "X" || grid[0] == "O" && grid[4] == "O" && grid[8] == "O";
+            bool diagonal2 = grid[6] == "X" && grid[4] == "X" && grid[2] == "X" || grid[6] == "O" && grid[4] == "O" && grid[2] == "O";
+
+            if (row1 || row2 || row3 || col1 || col2 || col3 || diagonal1 || diagonal2)
+            {
+                string winner = !player1Move ? "Player 1 WINS!!!" : "Player 2 WINS!!!";
+                Console.WriteLine(winner);
+                Replay(ref grid, ref player1Move, ref gameIsOn);
+            }
+        }
+
+        static void CheckDraw(ref string[] grid, ref bool player1Move, ref bool gameIsOn)
+        {
+            int validMoves = 9;
+
+            foreach (string square in grid)
+            {
+                if (square == "X" || square == "O")
+                {
+                    validMoves--;
+                }
+            }
+
+            if (validMoves == 0)
+            {
+                Console.WriteLine("It's a draw.");
+                Replay(ref grid, ref player1Move, ref gameIsOn);
+            }
+        }
+
+        static void Replay(ref string[] grid, ref bool player1Move, ref bool gameIsOn)
+        {
+            Console.WriteLine("For rematch enter 1, to exit press ENTER two times.");
+            gameIsOn = Console.ReadLine() == "1";
+
+            if (gameIsOn)
+            {
+                for (int i = 0; i < grid.Length; i++)
+                {
+                    grid[i] = (i + 1).ToString();
+                }
+
+                player1Move = true;
+                DrawGrid(grid);
+            }
         }
     }
 }
