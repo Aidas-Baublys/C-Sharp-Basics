ff4c4b42c7c27e58e15ff5ebfa4691db64d95e1a
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-05-01 11:35:45 +0300

    Created first event.

---
+++ HelloWorld2/DownloadHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+{
+    class DownloadHelper
+    {
+        public event EventHandler<EventArgs> FileDownload;
+
+        protected virtual void OnFileDownload()
+        {
+            if (FileDownload != null)
+            {
+                FileDownload(this, EventArgs.Empty);
+            }
+        }
+
+        public void Download(string file)
+        {
+            Console.WriteLine($"Downloading {file}...");
+            Thread.Sleep(4000);
+            OnFileDownload();
+        }
+    }
+}
--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -5,57 +5,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace HelloWorld2
 {
     class Program
     {
-        public delegate double PerfCalc(double a, double b);
-        public delegate string GetText(string name);
-
-        public static double Add(double a, double b)
-        {
-            Console.WriteLine(a + b);
-            return a + b;
-        }
-
-        public static double Divide(double a, double b)
-        {
-            Console.WriteLine(a / b);
-            return a / b;
-        }
-
-        public static double Sub(double a, double b)
-        {
-            Console.WriteLine(a - b);
-            return a - b;
-        }
-
         static void Main(string[] args)
         {
-            PerfCalc add = Add;
-            PerfCalc divide = Divide;
-            PerfCalc sub = Sub;
-            PerfCalc multiCalc = add + divide + sub;
-            multiCalc(10, 5);
-
-            PerfCalc a = (x, y) => x + y;
-
-            PerfCalc b = (x, y) => x * y;
-
-            GetText getText = delegate (string name)
-            {
-                return $"Hi, {name}.";
-            };
-
-            GetText text = (string name) => { return $"Nu nx {name}"; };
-
-            GetText t = name => $"Burka {name}";
-
-            Console.WriteLine(t("Kurwa"));
-            Console.WriteLine(b(5, 4));
-
+            var downloadHelper = new DownloadHelper();
+            downloadHelper.FileDownload += OnFileDownload;
+            downloadHelper.Download("dx");
+            
             Console.ReadKey();
+        }
+
+        public static void OnFileDownload(object source, EventArgs e)
+        {
+            Console.WriteLine("Unpacking...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Done nx.");
         }
     }
 }
